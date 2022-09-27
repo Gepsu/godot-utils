72965f8383406584027a5688ca6cb68fbf1bc347
Gepsu <joni.nyqvist@netikka.fi>
2022-09-27 23:02:12 +0300

    Added get_random_point_in_polygon

--- Utils.gd
+++ Utils.gd
@@ -25,7 +25,7 @@ static func get_files_in_folder(path: String, extension: String = "", full_path:
 	return files
 
 ## Makes a simple 2d array and fills it with whatever default value is set to
-static func make_2d_array(width: int, height: int, default_value = null) -> Array:
+static func create_2d_array(width: int, height: int, default_value = null) -> Array:
 	var array: Array
 	for x in width:
 		for y in height:
@@ -51,3 +51,49 @@ static func numbers_are_close(a: float, b: float, threshold: float) -> bool:
 static func clamp_to_viewport(viewport: Viewport, control: Control, margin: float = 0.0) -> void:
 	control.global_position.x = clamp(control.global_position.x, margin, viewport.get_visible_rect().end.x - control.size.x * 2 - margin)
 	control.global_position.y = clamp(control.global_position.y, margin, viewport.get_visible_rect().end.y - control.size.y * 2 - margin)
+
+## Blatantly stolen from: https://observablehq.com/@scarysize/finding-random-points-in-a-polygon Thank you!
+static func get_random_point_in_polygon(polygon: PackedVector2Array) -> Vector2:
+	# Get triangles
+	var points: Array = Geometry2D.triangulate_polygon(polygon)
+	var triangles: Array
+	while not points.is_empty():
+		triangles.append([polygon[points.pop_back()], polygon[points.pop_back()], polygon[points.pop_back()]])
+	
+	var get_triangle_area: Callable = func(tri: Array):
+		return 0.5 * (
+			(tri[1].x - tri[0].x) * (tri[2].y - tri[0].y) -
+			(tri[2].x - tri[0].x) * (tri[1].y - tri[0].y)
+		)
+	
+	# Generate distribution
+	var total_area = triangles.reduce(func(sum, tri): return sum + get_triangle_area.call(tri), 0)
+	var cumulative_distribution = []
+	
+	for i in triangles.size():
+		var last_value = 0 if i == 0 else cumulative_distribution[i - 1]
+		var next_value = last_value + get_triangle_area.call(triangles[i]) / total_area
+		cumulative_distribution.append(next_value)
+	
+	# Choose random triangle
+	var rnd = randf_range(0, 1)
+	var random_triangle = cumulative_distribution.filter(func(i): return i > rnd).front()
+	var random_index = cumulative_distribution.find(random_triangle)
+	
+	# Calculate random point
+	var wb = randf_range(0, 1)
+	var wc = randf_range(0, 1)
+	
+	if wb + wc > 1:
+		wb = 1 - wb
+		wc = 1 - wc
+	
+	var tri = triangles[random_index]
+	var rb_x = wb * (tri[1].x - tri[0].x)
+	var rb_y = wb * (tri[1].y - tri[0].y)
+	var rc_x = wc * (tri[2].x - tri[0].x)
+	var rc_y = wc * (tri[2].y - tri[0].y)
+	
+	var r_x = rb_x + rc_x + tri[0].x
+	var r_y = rb_y + rc_y + tri[0].y
+	return Vector2(r_x, r_y)
