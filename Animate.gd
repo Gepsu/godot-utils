7823034c8ba9f600fce9f5b8cdbced682bb75f40
Gepsu <joni.nyqvist@netikka.fi>
2022-10-05 09:06:19 +0300

    Created Animate class for simple tween animations

---
+++ Animate.gd
@@ -0,0 +1,16 @@
+class_name Animate extends Resource
+
+static func _is_valid(node) -> bool:
+	if not (node is Node2D or node is Control):
+		printerr("Animate node has to be Node2D or Control")
+		return false
+	return true
+
+static func bounce_scale(node, _signal: Signal, target_scale: Vector2, duration := 0.1, trans: int = Tween.TRANS_BOUNCE) -> void:
+	if not _is_valid(node): return
+	
+	_signal.connect(func(): 
+		var tween: Tween = node.create_tween()
+		tween.tween_property(node, "scale", target_scale, duration)
+		tween.set_trans(trans)
+	)
