fe6cc7f94cfcb5d37506dbe88de2aeda55e5ceaf
Gepsu <joni.nyqvist@netikka.fi>
2022-10-13 06:47:49 +0300

    Added some presets

--- Animate.gd
+++ Animate.gd
@@ -1,10 +1,49 @@
 class_name Animate extends Resource
 
-static func property(object: Object, property: NodePath, final_val, duration := 1.0, trans := Tween.TRANS_LINEAR) -> AnimationData:
+static func property(object, property, final_val, duration := 1.0, trans := Tween.TRANS_LINEAR, ease := Tween.EASE_IN_OUT) -> AnimationData:
 	var anim_callable := func():
 		var tween: Tween = object.create_tween()
 		tween.tween_property(object, property, final_val, duration)
 		tween.set_trans(trans)
+	return AnimationData.new(anim_callable)
+
+static func custom(anim: Callable) -> AnimationData:
+	return AnimationData.new(anim)
+
+static func pulse_scale(object, start_val, end_val, stop_signal: Signal, duration := 1.0, trans := Tween.TRANS_SINE, ease := Tween.EASE_IN_OUT) -> AnimationData:
+	var anim_callable := func():
+		var tween: Tween = object.create_tween()
+		tween.tween_property(object, "scale", end_val, duration)
+		tween.tween_property(object, "scale", start_val, duration)
+		tween.set_loops(0)
+		tween.set_trans(trans)
+		stop_signal.connect(tween.kill)
+	return AnimationData.new(anim_callable)
+
+static func swing(object, rotation_degrees: float, stop_signal: Signal, duration := 1.0, trans := Tween.TRANS_SINE, ease := Tween.EASE_IN_OUT) -> AnimationData:
+	var anim_callable := func():
+		var tween: Tween = object.create_tween()
+		tween.tween_property(object, "rotation", deg_to_rad(-abs(rotation_degrees)), duration / 2)
+		tween.set_trans(trans)
+		tween.tween_callback(func():
+			tween = object.create_tween()
+			tween.tween_property(object, "rotation", deg_to_rad(abs(rotation_degrees)), duration)
+			tween.tween_property(object, "rotation", deg_to_rad(-abs(rotation_degrees)), duration)
+			tween.set_loops(0)
+			tween.set_trans(trans)
+			stop_signal.connect(tween.kill)
+		)
+		stop_signal.connect(tween.kill)
+	return AnimationData.new(anim_callable)
+
+static func rainbow(object: CanvasItem, stop_signal: Signal, saturation := 1.0, duration := 1.0) -> AnimationData:
+	var anim_callable := func():
+		var tween: Tween = object.create_tween()
+		tween.tween_property(object, "modulate:h", 0, 0)
+		tween.tween_property(object, "modulate:s", saturation, 0.1)
+		tween.tween_property(object, "modulate:h", 1, duration)
+		tween.set_loops(0)
+		stop_signal.connect(tween.kill)
 	return AnimationData.new(anim_callable)
 
 class AnimationData extends Resource:
@@ -18,12 +57,3 @@ class AnimationData extends Resource:
 	
 	func on_signal(_signal: Signal) -> void:
 		_signal.connect(anim_callable)
-
-#static func scale(node, _signal: Signal, target_scale: Vector2, duration := 0.1, trans: int = Tween.TRANS_BOUNCE) -> void:
-#	if not _is_valid(node): return
-#
-#	_signal.connect(func(): 
-#		var tween: Tween = node.create_tween()
-#		tween.tween_property(node, "scale", target_scale, duration)
-#		tween.set_trans(trans)
-#	)
